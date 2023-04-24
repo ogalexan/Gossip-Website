fe57aeda6beae4b86fd6b8bd27f86e96dbd3c52e
rachelamster <122999955+rachelamster@users.noreply.github.com>
2023-04-24 11:28:23 -0400

    Final Project: created template

--- footer.php
+++ footer.php
@@ -1,3 +1,5 @@
 <footer>
-        <p><a href= "https://ramster.w3.uvm.edu/cs008/">Site map (Main index)</a></p>
+        <p><a href= 'https://ramster.w3.uvm.edu/cs008'>Site map</a></p>
+        <p><a href= '//ramster.w3.uvm.edu/cs008/final'>Rachel Amster - Section A</a></p>
+        <p><a href= '//ogalexan.w3.uvm.edu/cs008/final'>Rachel Amster - Section A</a></p>
     </footer>
