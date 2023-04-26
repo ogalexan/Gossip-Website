b430a058599f1153798204f50bd64d73817d83b3
ogalexan <123039321+ogalexan@users.noreply.github.com>
2023-04-26 11:18:02 -0400

    Add files via upload

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 <footer>
         <p><a href= 'https://ramster.w3.uvm.edu/cs008'>Site map</a></p>
         <p><a href= '//ramster.w3.uvm.edu/cs008/final'>Rachel Amster - Section A</a></p>
-        <p><a href= '//ogalexan.w3.uvm.edu/cs008/final'>Rachel Amster - Section A</a></p>
+        <p><a href= '//ogalexan.w3.uvm.edu/cs008/final'>Olivia Alexander  - Section A</a></p>
     </footer>
