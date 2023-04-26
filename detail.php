5a56fd0870ecfae298f7fd9a7d3fbd7349f475fd
rachelamster <122999955+rachelamster@users.noreply.github.com>
2023-04-26 11:10:30 -0400

    Final: upload files

--- detail.php
+++ detail.php
@@ -2,39 +2,46 @@
   include 'top.php';
 ?>
 <main>
-    <h1>This Week's Stories brought to you by Maggie Swanborn</h1>
+    <h1>This Week's Stories brought to you by Rachel Amster and Olivia Alexander</h1>
 
     <section>
         <h2>Maggie Swanbord</h2>
         <figure class="rounded">
-        <img class="rounded" alt="Travis Rice" src="image/travisrice.jpg">
-        <figcaption>Author</figcaption>
+        <img class="rounded" alt="Gossip" src="images/gossip.jpg">
+        <figcaption>You don't exist until they talk about you</figcaption>
 </figure>
     </section>
 
     <section>
-        <h2>The Academy Awards</h2>
-        <p>Aka the Oscars. The finale to the awards season is this Sunday (today) 
-            7pm CDT and will be hosted by Jimmy Kimmel (yawn). I decided to fast track the 
-            first newsletter so I could give my official guide to Oscars 2023. So without 
-            further ado…
-        </p>
-
-        <p>colspan="3">Source: <cite><a hred="https://en.wikipedia.org/wiki/Snowboarding" 
-                target="_blank">https://en.wikipedia.org/wiki/Snowboarding</a></cite></p>   
-
+        <h2>Everything You Need to Know About the 2023 Met Gala</h2>
+        <p>Where and when is the Gala?
+            The 2023 Met Gala will be held in New York City on May 1 at 6:30 PM. 
+            What is the theme?
+            The theme is “In Honor of Karl" and will serve as a celebration of Karl Lagerfeld’s work throughout his lifetime.  
+            Who are the hosts?
+            Penélope Cruz, Dua Lipa, Roger Federer and Michaela Coel.
+            Can't wait to witness which controversies we'll see this year!</p>
     </section>
 
     <section>
-        <h2>Some of the greats</h2>
-        <p>There are many super talented snowboarders out there and checking anyone out on your local mountain can help your form. However, these are a few snowboarders to look up if only merely for inspiration. 
-            Shaun White- a three-time Olympic gold medalist 
-            Travis Rice- created an incredible film, 'Transcendence'
-            Gretch Bleir- four time gold medalist and first female rider to perfect 'The Crippler'
-        </p>
-        
+        <h2>But I gotta keep an eye out for Selener</h2>
+        <p>Hailey Beiber being called a mean girl. Ouch! Now, it all started with a TikTok when fans assumed that Hailey posted a vieo “shading” Sel. Given Selena and Justin's history, I wonder if hubby will ever step in and start talkin</p>
+
+        <p>Kylie Jenner also being called a mean girl! What in the world is happeneing! In the wake of Selena mentioning that she over-laminated her eyebrows on TikTok, fans spiral thanks to Kylie posting a photo of herself with the words “This was an accident???” over her brows.</p>
     </section>
 
+    <section>
+        <h2>Taylor Swift and Joe Alwyn break up after 6 years</h2>
+        <p>The one couple everyone (at least I) was rooting for has broken up. I wonder how Swift is holding up being that she is currently on her “Eras Tour.” They managed to keep their relationship mostly under wraps, but in true Taylor Swift style, she dropped some songs about him. Alwyn was also credited on a number of songs from Swift’s record breaking albums “folklore” and “evermore."</p>
+    </section>
+
+    <section>
+        <h2>Sofia Richie and Elliot Grainge are married!</h2>
+        <p>The 24-year-old model and designer, who is also the youngest child of legendary singer Lionel Richie (nepo baby alert!) had her star-studded wedding at Hotel du Cap-Eden-Roc in the dreamy South of France. From Justin Bieber to Scott Disick, she's finally found the one. And now she's our new it girl</p>
+
+        <p>colspan="3">Source: <cite><a hred="https://hollywoodlife.com/wp-content/uploads/2023/04/Sofia-Richie-Wedding-Dress-MEGA-embed-1.jpg" 
+        target="_blank">https://hollywoodlife.com/wp-content/uploads/2023/04/Sofia-Richie-Wedding-Dress-MEGA-embed-1.jpg</a></cite></p>   
+    </section>  
 </main>
 
 <?php
