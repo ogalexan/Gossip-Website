59abc9a3a640e48be7e223e168380c644355325c
rachelamster <122999955+rachelamster@users.noreply.github.com>
2023-05-11 11:41:06 -0400

    Add files via upload

--- final/index.php
+++ final/index.php
@@ -1,1 +1,106 @@
+<?php
+  include 'top.php';
 
+$actorsTable = array(
+    array('Jennifer Lawrence', 'Causeway', 'Don\'t Look Up', 'The Hunger Games'),
+    array('Timothee Chalamet', 'Little Women', 'Lady Bird', 'Call Me By Your Name'),
+    array('Paul Mescal', 'Carmen', 'The Lost Daughter', 'Normal People'),
+    array('Kate Blanchett', 'Tar', 'Where\'d You Go Bernadette', 'Ocean\'s 8'),
+    array('Angela Bassett', 'Black Panther: Wakanda Forever', 'Wendell & Wild ','Otherhood') 
+);
+?>
+
+
+    <h1>Drama on the Hill</h1>
+    
+    <main>
+    <section>
+        <h2>Depiction of Politics in Film</h2>
+        <p>Politics has played a role in movies since the dawn of cinema. They play a key role in the story line for films of all types and often reflect where we are as a nation. Political films portray various society conditions and partisan perspectives. Normally,people are drawn to political films because they are a reflection of the society we live in.</p> 
+    </section>
+
+    <section>
+        <h3></h3>
+        <figure class="rounded">
+            <img class="rounded" alt="Oscar Isaac" src="images/show-me.jpg">
+            <figcaption><cite><a href="https://www.pastemagazine.com/tv/tv-politics/the-best-political-tv-shows-of-all-time"
+            target="_blank">Oscar Isaac starring in Show Me a Hero</a></cite></figcaption>
+        </figure>
+    </section>
+
+
+    <section>
+        <h2>Looking for a new political drama?</h2>
+        <p>Here's a list of some classic political movies and tv shows.</p>
+
+        <ol>
+            <li>Veep
+                <ol>
+                    <li>A comedy series about the Vice President of the United States. Each episode follows her day-to-day life as she puts out political fires.</li>
+            </ol>
+        </li>
+        <ol>
+            <li>The West Wing
+                <ol>
+                    <li>A drama that follows the inside lives of staffers in the West Wing of the White House.</li>
+            </ol>
+        </li>
+        <ol>
+            <li>The Wire
+                <ol>
+                    <li>A drama about crime, law enforcement, politics, education and media in Baltimore as it follows a team of cops and the criminals they are after.</li>
+            </ol>
+        </li>
+        <ol>
+            <li>Parks and Recreation
+                <ol>
+                    <li>An American political satire sitcom about the antics of an Indiana town's public officials as they pursue projects to make their city a better place.</li>
+            </ol>
+        </li>
+        <ol>
+            <li>The Good Fight
+                <ol>
+                    <li>A hysterical legal drama about lawyers at the end of history.</li>
+            </ol>
+        </li>
+    </section>
+
+    <section>
+    <h2>Some greatest <?php print count($actorsTable); ?> hits</h2>
+        <table>
+            <tr>
+                <th>Your Fav Actor/Actress! </th>
+                <th>Top Hits!</th>
+                <th></th>
+                <th></th>
+            </tr>
+    <?php
+    foreach ($actorsTable as $actorTable) {
+        print '<tr>';
+        print '<td>' . $actorTable [0] . '</td>';
+        print '<td>' . $actorTable [1] . '</td>';
+        print '<td>' . $actorTable [2] . '</td>';
+        print '<td>' . $actorTable [3] . '</td>';
+        print '</tr>' . PHP_EOL;
+    }
+    ?>
+            <tr>
+                <td colspan="5"><cite>Data from IMbD <a href="https://www.imdb.com/news/ni63707932" target="blank">Editors Picks</cite></a>
+            </tr>
+    </table>
+</section>
+
+<section>
+    <h3></h3>
+    <figure class="rounded">
+        <img class="rounded" alt="Veep" src="images/veep.jpg">
+        <figcaption><a
+        target="_blank">Veep Propaganda</a></figcaption>
+</figure>
+</section>
+
+</main>
+
+<?php
+include 'footer.php';
+?>
