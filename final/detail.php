e56ad92172429eea78a8f0a9dd450d42038796a9
rachelamster <122999955+rachelamster@users.noreply.github.com>
2023-05-11 11:48:55 -0400

    Final Project: worked on the detail page

---
+++ final/detail.php
@@ -0,0 +1,53 @@
+<?php
+  include 'top.php';
+?>
+
+    <h1>This Week's Stories brought to you by Rachel Amster and Olivia Alexander</h1>
+    
+    <main>
+    <section>
+        <figure class="rounded">
+        <img class="rounded" alt="Gossip" src="images/gossip.jpg">
+        <figcaption>You don't exist until they talk about you</figcaption>
+</figure>
+    </section>
+
+    <section>
+        <h2>Everything You Need to Know About the 2023 Met Gala</h2>
+        <p>Where and when is the Gala?</p>
+        <p>According to tradition, the 2023 Met Gala will be held in New York City on May 1 at 6:30 PM.</p> 
+        <p>What is the theme?</p>
+        <p>The theme is “In Honor of Karl" and will serve as a celebration of Karl Lagerfeld’s work throughout his lifetime. Karl Lagerfeld was a world-renowned designer who led fashion industries such as Chanel and Fendi.</p>
+        <p>Who are the hosts?</p>
+        <p>This year's offcial Met Gala co-hosts are, Penélope Cruz, Dua Lipa, Roger Federer, Michaela Coel, and of course Vogue's Anna Wintour.</p>
+        <p>Can't wait to witness which controversies we'll see this year!</p>
+    </section>
+
+    <section>
+        <h2>But I gotta keep an eye out for Selener</h2>
+        <p>Hailey Beiber being called a mean girl. Ouch! Now, it all started with a TikTok when fans assumed that Hailey posted a vieo “shading” Sel. Given Selena and Justin's history, I wonder if hubby will ever step in and start talkin</p>
+
+        <p>Kylie Jenner also being called a mean girl! What in the world is happeneing! In the wake of Selena mentioning that she over-laminated her eyebrows on TikTok, fans spiral thanks to Kylie posting a photo of herself with the words “This was an accident???” over her brows.</p>
+    </section>
+
+    <section>
+        <h2>Taylor Swift and Joe Alwyn break up after 6 years</h2>
+        <p>The one couple everyone (at least I) was rooting for has broken up. I wonder how Swift is holding up being that she is currently on her “Eras Tour.” They managed to keep their relationship mostly under wraps, but in true Taylor Swift style, she dropped some songs about him. Alwyn was also credited on a number of songs from Swift’s record breaking albums “folklore” and “evermore."</p>
+    </section>
+
+    <section>
+        <h2>Sofia Richie and Elliot Grainge are married!</h2>
+        <p>The 24-year-old model and designer, who is also the youngest child of legendary singer Lionel Richie (nepo baby alert!) had her star-studded wedding at Hotel du Cap-Eden-Roc in the dreamy South of France. From Justin Bieber to Scott Disick, she's finally found the one. And now she's our new it girl</p>
+    </section> 
+
+    <section>
+        <figure class="rounded">
+        <img class="rounded" alt="Gossip" src="images/wedding.jpg">
+        <figcaption>The Royal Wedding (for the U.S.)</figcaption>
+        </figure>  
+    </section>  
+</main>
+
+<?php
+  include 'footer.php';
+?>
